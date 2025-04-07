891ed69388f4e485bf1c7531052854a141f75269
davidnguyennq <nguyennhatquan.dev@gmail.com>
2025-04-07 10:02:25 +0700

    Trangchu

--- Manager/Controllers/HomeController.cs
+++ Manager/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Manager.Controllers
 {
@@ -22,6 +18,10 @@ namespace Manager.Controllers
         }
 
         public ActionResult TaoHangHoa()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
         {
             return View();
         }
